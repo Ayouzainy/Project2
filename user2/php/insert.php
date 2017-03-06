c9fd35e04d090362b268ad4738000f340e7281c0
fern1994 <hemhongsa.1994@gmail.com>
2017-03-06 17:27:19 +0700

    delete user2

--- user2/php/insert.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$postData = file_get_contents("php://input");
-$data = json_decode($postData);
-include 'dbConfig.php';
-
-$sql = "INSERT INTO employee (name, dept, area, status, contact, salary) VALUES ('$data->name', '$data->dept', '$data->area', '$data->status', '$data->contact', '$data->salary')";
-$qry = $conn->query($sql);
-$conn->close();
- ?>
--- user2/php/update.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$postData = file_get_contents("php://input");
-$data = json_decode($postData);
-include 'dbConfig.php';
-
-$sql = "UPDATE employee SET name = '$data->name', dept = '$data->dept', area = '$data->area', status = '$data->status', contact = '$data->contact', salary = '$data->salary' WHERE id = $data->id";
-$qry = $conn->query($sql);
-$conn->close();
- ?>
